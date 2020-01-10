ddf1fc2b8ede8f96ad3f5581389d0dce2b2d9756
xiyouzhanglong <59726012+xiyouzhanglong@users.noreply.github.com>
2020-01-10 17:44:41 +0800

    修改后提交2222

--- eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
+++ eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
@@ -15,7 +15,7 @@ public class EladminSystemApplicationTests {
     }
 
     public static void main(String[] args) {
-        System.out.println("--测试---");
+        System.out.println("--测试222---");
     }
 }
 
